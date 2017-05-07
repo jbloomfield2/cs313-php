5ba1a2ce390fd2cd1749e7bb04910bf3ee44f6b3
jbloomfield2 <blo15016@byui.edu>
2017-05-06 21:41:41 -0600

    small update to assignments page

--- web/assignments.php
+++ web/assignments.php
@@ -1,8 +1,11 @@
 <!DOCYTPE html>
 <html>
     <head>
+        <link rel="stylesheet" href="style.css">
         <title>CS313 Assignments</title>
     </head>
     <body>
-    <h2>Coming Soon</h2></body>
+    <h2>Under construction</h2>
+    <br><a href="cart/shop.php">Shopping Cart</a>
+    </body>
 </html>
